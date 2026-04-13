b7c39880b33c51a6442c3b22c60a5ed7974d07d3
gdjrn <gabrielnougues@gmail.com>
2026-04-13 16:24:52 +0200

    mensaje

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hnhjiuhiuhuihDocker CI/CD funcionando correctamenteeeee");
+  res.end("hnhiuhuihDocker CI/CD funcionando correctamenteeeee");
 });
 
 server.listen(3000, () => {
