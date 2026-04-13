fc6adf4723010d689db76d3d9ca798e7dcbc9e7b
gdjrn <gabrielnougues@gmail.com>
2026-04-13 16:10:09 +0200

    firstttttt commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Docker CI/CD funcionando correctamente");
+  res.end("Docker CI/CD funcionando correctamenteeeee");
 });
 
 server.listen(3000, () => {
